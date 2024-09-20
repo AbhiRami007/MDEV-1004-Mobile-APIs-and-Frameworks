efc5cb73430fd0e448ccee724f99b081750a6012
Abhirami007 <psabhirami015@gmail.com>
2024-09-20 09:48:54 -0400

    printing the paginated response of REST API on console

--- lab2/REST/script.js
+++ lab2/REST/script.js
@@ -2,3 +2,7 @@ fetch("https://swapi.dev/api/people?page=1")
 .then(function(response) {
     console.log(response.json())
 })
+.then(function(jsonData) {
+    console.log("REST API Response - Paginated")
+    console.log(jsonData)
+})
