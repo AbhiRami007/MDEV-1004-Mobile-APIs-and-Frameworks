23d1c97ded59d178416b798a8eca341996def541
Abhirami007 <psabhirami015@gmail.com>
2024-09-20 09:45:25 -0400

    Returning the response and showing in console

--- lab2/GRAPHQL/script.js
+++ lab2/GRAPHQL/script.js
@@ -1,5 +1,8 @@
 fetch("https://swapi-graphql.netlify.app/.netlify/functions/index?query={allPeople{people{id, name, birthYear, gender}}}")
 .then(function(response) {
-    console.log(response.json())
+    return response.json();
 })
-
+.then(function(jsonData) {
+    console.log("GraphQl Response")
+    console.log(jsonData)
+})
