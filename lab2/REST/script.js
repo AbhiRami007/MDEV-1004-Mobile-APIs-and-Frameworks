6168502480597ba8dbc8d0271e2dbf9b6b069c26
Abhirami007 <psabhirami015@gmail.com>
2024-09-20 09:49:41 -0400

    returning the response json

--- lab2/REST/script.js
+++ lab2/REST/script.js
@@ -1,6 +1,6 @@
 fetch("https://swapi.dev/api/people?page=1")
 .then(function(response) {
-    console.log(response.json())
+    return response.json()
 })
 .then(function(jsonData) {
     console.log("REST API Response - Paginated")
