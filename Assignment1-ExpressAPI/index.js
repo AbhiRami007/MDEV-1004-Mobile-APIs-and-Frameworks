06e11534c04d03babbeda659c40648254ff733b1
Abhirami007 <psabhirami015@gmail.com>
2024-09-26 15:30:49 -0400

    adding controller and route to get all recipes

--- Assignment1-ExpressAPI/index.js
+++ Assignment1-ExpressAPI/index.js
@@ -1,3 +1,10 @@
+/* 
+	File: index.js
+	Student's Name: Abhirami Pradeep Susi
+	Student ID: 200589663
+	Date: 26 Sep 2024
+*/
+
 //import express
 const bodyParser = require("body-parser");
 const express = require("express");
--- Assignment1-ExpressAPI/routes/recipeRoute.js
+++ Assignment1-ExpressAPI/routes/recipeRoute.js
@@ -1,8 +1,18 @@
+/* 
+	File: recipeRoute.js
+	Student's Name: Abhirami Pradeep Susi
+	Student ID: 200589663
+	Date: 26 Sep 2024
+*/
+
 const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipeController');
 
-// GET route for all recipes
+// POST route for creating recipe
 router.post('/recipes', recipeController.createRecipes);
+
+// GET route for all recipes
+router.get('/recipes', recipeController.getAllRecipes);
 
 module.exports = router;
