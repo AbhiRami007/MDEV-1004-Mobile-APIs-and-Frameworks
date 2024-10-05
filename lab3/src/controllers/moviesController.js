96bea7a01700564635fcfa58f9e2749f248b920e
Abhirami007 <psabhirami015@gmail.com>
2024-10-05 17:28:41 -0400

    adding import movies controller

---
+++ lab3/src/controllers/moviesController.js
@@ -0,0 +1,32 @@
+/* 
+  File: moviesController.js
+  Student's Name: Abhirami Pradeep Susi
+  Student ID: 200589663
+  Date: 05 Oct 2024
+*/
+
+const Movie = require("../models/moviesModel");
+const fs = require("fs");
+
+/**
+ * Function to import movies to database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {void} - Returns success message on movies creation or an error message.
+ */
+
+exports.importMovies = async (req, res) => {
+  try {
+    // Read data from movies.json
+    const data = JSON.parse(fs.readFileSync("./movies.json", "utf-8"));
+    const count = await Movie.countDocuments();
+    if (count === 0) {
+      await Movie.create(data);
+      res.status(200).send("Data successfully imported to mongoDB");
+    } else {
+      res.status(200).send("Data already exists, skipping import");
+    }
+  } catch (e) {
+    res.status(500).send("Error importing data");
+  }
+};
