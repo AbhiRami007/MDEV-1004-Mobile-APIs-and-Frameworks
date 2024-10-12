eb04597a668aabb39eb98a8bf35d51f3bda52c4e
Abhirami007 <psabhirami015@gmail.com>
2024-10-12 16:30:24 -0400

    hashing password on save and login API

--- Assignment2-ExpressAPI/controllers/userController.js
+++ Assignment2-ExpressAPI/controllers/userController.js
@@ -6,14 +6,16 @@
 */
 
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
 
 /**
  * Function to create new user.
  * @param {Object} req - The request object.
  * @param {Object} res - The response object.
  * @returns {void} - Returns success message with username user creation or an error message.
- */
-// Register a new user (with password hashing middleware applied)
+ * @description - Register a new user (with password hashing middleware applied)
+*/
+
 exports.registerUser = async (req, res) => {
     const { username, password } = req.body;
 
@@ -24,8 +26,11 @@ exports.registerUser = async (req, res) => {
             return res.status(400).json({ message: 'User already exists' });
         }
 
+				//Hash incoming password before saving
+				const hashedPassword = await bcrypt.hash(password, 10)
+
         // Create new user
-        const newUser = new User({ username, password });
+        const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
 
         // Return user data excluding the password
@@ -34,6 +39,32 @@ exports.registerUser = async (req, res) => {
             user: {
                 username: newUser.username
             }
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
+
+/**
+ * Function to create new user.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {void} - Returns success message on login or an error message.
+ * @description - Login a user with password
+*/
+
+exports.loginUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Find user by username
+        const user = await User.findOne({ username });
+				const matchPassword = await bcrypt.compare(password, user.password)
+        if (!user || !matchPassword) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+			 	res.status(200).json({
+            message: 'Login successful',
         });
     } catch (error) {
         res.status(500).json({ message: 'Server error', error });
--- Assignment2-ExpressAPI/routes/userRoutes.js
+++ Assignment2-ExpressAPI/routes/userRoutes.js
@@ -12,4 +12,7 @@ const userController = require('../controllers/userController');
 // Register a new user
 router.post('/register', userController.registerUser);
 
+// Log in a user
+router.post('/login', userController.loginUser);
+
 module.exports = router;
