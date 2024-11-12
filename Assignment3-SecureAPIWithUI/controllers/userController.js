7036fe61d0489bfb3c7e908f82f90d13e868a2c0
Abhirami007 <psabhirami015@gmail..com>
2024-11-12 16:53:45 -0500

    updating jwt token generation for login

--- Assignment3-SecureAPIWithUI/controllers/userController.js
+++ Assignment3-SecureAPIWithUI/controllers/userController.js
@@ -5,8 +5,9 @@
   Date: 11 Nov 2024
 */
 
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 /**
  * Function to create new user.
@@ -14,35 +15,36 @@ const bcrypt = require('bcryptjs');
  * @param {Object} res - The response object.
  * @returns {void} - Returns success message with username user creation or an error message.
  * @description - Register a new user (with password hashing middleware applied)
-*/
+ */
 
 exports.registerUser = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        // Check if user already exists
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-				//Hash incoming password before saving
-				const hashedPassword = await bcrypt.hash(password, 10)
-
-        // Create new user
-        const newUser = new User({ username, password: hashedPassword });
-        await newUser.save();
-
-        // Return user data excluding the password
-        res.status(201).json({
-            message: 'User registered successfully',
-            user: {
-                username: newUser.username
-            }
-        });
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error });
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    //Hash incoming password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+
+    // Return user data excluding the password
+    res.status(201).json({
+      message: "User registered successfully",
+      data: {
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
 };
 
 /**
@@ -51,24 +53,35 @@ exports.registerUser = async (req, res) => {
  * @param {Object} res - The response object.
  * @returns {void} - Returns success message on login or an error message.
  * @description - Login a user with password
-*/
+ */
 
 exports.loginUser = async (req, res) => {
-    const { username, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        // Find user by username
-        const user = await User.findOne({ username });
-				const matchPassword = await bcrypt.compare(password, user.password)
-        if (!user || !matchPassword) {
-            return res.status(401).json({ message: 'Invalid username or password' });
-        }
-			 	res.status(200).json({
-            message: 'Login successful',
-        });
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error });
+  try {
+    // Find user by username
+    const user = await User.findOne({ email });
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if (!user || !matchPassword) {
+      return res.status(401).json({ message: "Invalid username or password" });
     }
+
+    //Create token for the user
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "10m",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      data: { username: user.username, email: email, token },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
 };
 
 /**
@@ -77,7 +90,7 @@ exports.loginUser = async (req, res) => {
  * @param {Object} res - The response object.
  * @returns {void} - Returns success message on logout.
  * @description - Logout user
-*/
+ */
 exports.logoutUser = (req, res) => {
-    res.json({ message: "Logged out successfully" });
+  res.json({ message: "Logged out successfully" });
 };
