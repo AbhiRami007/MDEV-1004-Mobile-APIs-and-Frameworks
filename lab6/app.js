efac99e884abdd14e551b29758aa6631ea374c8f
Abhirami007 <psabhirami015@gmail..com>
2024-11-29 18:08:57 -0500

    adding app.js to connect to server

---
+++ lab6/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require('path');
+const cors = require('cors');
+
+const firebaseRoutes = require("./routes/firebaseRoutes");
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(cors());
+// Middleware
+app.use(express.json());
+// Routes
+app.use("/firebase", firebaseRoutes); // Include Firebase authentication routes
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
