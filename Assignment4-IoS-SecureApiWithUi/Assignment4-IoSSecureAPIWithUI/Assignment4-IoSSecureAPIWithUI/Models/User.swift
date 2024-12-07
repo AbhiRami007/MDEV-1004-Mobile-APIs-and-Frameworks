96333fc2eb75f17ccf91fe3f407d1929f8f8e105
Abhirami007 <psabhirami015@gmail..com>
2024-12-07 01:17:14 -0500

    adding user model

---
+++ Assignment4-IoS-SecureApiWithUi/Assignment4-IoSSecureAPIWithUI/Assignment4-IoSSecureAPIWithUI/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Assignment4-IoSSecureAPIWithUI
+//
+//  Created by Abhirami Pradeep Susi on 2024-12-07.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let email: String
+    let password: String
+}
