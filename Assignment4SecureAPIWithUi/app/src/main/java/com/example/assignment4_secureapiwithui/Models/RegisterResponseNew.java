03d2d99cc00f26714a482a40849fc79f7f90360c
Abhirami007 <psabhirami015@gmail..com>
2024-12-07 17:22:10 -0500

    register response models added

---
+++ Assignment4SecureAPIWithUi/app/src/main/java/com/example/assignment4_secureapiwithui/Models/RegisterResponseNew.java
@@ -0,0 +1,14 @@
+package com.example.assignment4_secureapiwithui.Models;
+
+public class RegisterResponseNew {
+    private String message;
+
+    // Getters and Setters
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
